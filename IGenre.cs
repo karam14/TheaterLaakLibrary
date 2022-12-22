cb58e348ab0f04cf2293f2e2ba36f08046cda22f
Karam <krm-=011@hotmail.com>
2022-12-22 01:35:32 +0100

    Using fix

--- IGenre.cs
+++ IGenre.cs
@@ -1,6 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TheaterLaakLibrary;
 public interface IGenre
 {
+    [Key]
     public int Id { get; set; }
     public string Naam { get; set; }
 }
--- IKaart.cs
+++ IKaart.cs
@@ -1,6 +1,8 @@
-﻿namespace TheaterLaakLibrary;
+﻿using System.ComponentModel.DataAnnotations;
+namespace TheaterLaakLibrary;
 public interface IKaart
 {
+    [Key]
     public int Id { get; set; }
     public string Waarheidsmerk { get; set; }
 }
